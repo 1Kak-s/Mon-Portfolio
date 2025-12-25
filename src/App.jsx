49eccbc71ade6b3b4b84907a66c94f1826da2d02
Goeffi_m <goeffi_m@etna-alternance.net>
2025-12-25 14:43:15 +0100

    feat: add base layout and theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import Portfolio from './Portfolio'
-
 function App() {
-  return <Portfolio />
+  return (
+    <div className="min h-screen bg-slate-900 text-white">
+
+
+      <main>
+
+        <section className="h-screen flex-items-center justify-center">
+          <h1 className="text-5xl font-bold"> Maxime <span className="text-rose-500">Goëffier</span></h1>
+        </section>
+
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
