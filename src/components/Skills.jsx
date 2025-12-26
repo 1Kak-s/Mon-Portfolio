f3d9611544d1289fd73db17f9174539d129a0156
Goeffi_m <goeffi_m@etna-alternance.net>
2025-12-26 22:10:53 +0100

    feat: add lucide-react dependency and update icon in Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -58,7 +58,7 @@ function Skills() {
     const skillCategories = [
     {
         title: 'Langages',
-        icon: '💻',
+        icon: '</>',
         skills: [
             { name: 'Python', level: 50 },
             { name: 'JavaScript/TypeScript', level: 45 },
