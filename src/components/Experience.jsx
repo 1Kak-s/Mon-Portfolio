968e5cd2c97b0b1cd1a63ae252b7a310589d7577
Goeffi_m <goeffi_m@etna-alternance.net>
2026-01-03 12:53:42 +0100

    feat: add Experience component to showcase professional experiences

---
+++ src/components/Experience.jsx
@@ -0,0 +1,42 @@
+import { icons } from "lucide-react";
+
+function Experience() {
+
+    const experiences = [
+        {
+            period: "2024 - 2027",
+            title: "Étudiant",
+            company: "ETNA",
+            location: "Ivry-sur-Seine, France",
+            description: "Formation en développement logiciel, couvrant divers langages de programmation, frameworks et méthodologies de développement.",
+            type: "formation",
+            icons: "🎓",
+        },
+        {
+            period: "2022 - 2024",
+            tittle: "Directeur adjoint",
+            company: "Canada Goose",
+            location : "Paris, France",
+            description: "Gestion des opérations quotidiennes, supervision du personnel (20+ employés), formation et développement des compétences, gestion des stocks et des relations avec les clients.",
+            type: "Travail",
+            icons: "🏬",
+        },
+        {
+            period: "2019 - 2021",
+            title: "Responsable Stock",
+            company: "Nike - Citadium",
+            location : "Paris, France",
+            description: "Gestion des stocks, prévention des pertes, organisation des livraisons, développement des processus de sécurité.",
+            type: "Travail",
+            icons: "📦",
+        },
+        {
+            period: "2017 - 2019",
+            tittle: "vendeur polyvalent",
+            company:"Nike - Citadium",
+            location : "Paris, France",
+            description: "Service à la clientèle,  présentation des produits, assistance aux clients dans leurs choix.",
+            type: "Travail",
+            icons: "👟",
+        }
+    ];
