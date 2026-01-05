9446563554088d56dfc6cf85b31ef0fe2f1c2579
Goeffi_m <goeffi_m@etna-alternance.net>
2026-01-05 09:15:37 +0100

    refactor: remove unused sections from App component for cleaner structure

--- src/App.jsx
+++ src/App.jsx
@@ -19,30 +19,6 @@ function App() {
         <Projects />
         <Experience />
         <Contact />
-
-        <section className="h-screen flex items-center justify-center">
-          <h1 className="text-5xl font-bold"> Maxime <span className="text-rose-500">Goëffier</span></h1>
-        </section>
-
-        <section id="about" className="h-screen flex items-center justify-center">
-          <h2 className="text-5xl font-bold">À propos</h2>
-        </section>
-
-        <section id="skills" className="h-screen flex items-center justify-center">
-          <h2 className="text-5xl font-bold">Compétences</h2>
-        </section>
-
-        <section id="projects" className="h-screen flex items-center justify-center">
-          <h2 className="text-5xl font-bold">Projets</h2>
-        </section>
-
-        <section id="parcours" className="h-screen flex items-center justify-center">
-          <h2 className="text-5xl font-bold">Parcours</h2>
-        </section>
-
-        <section id="contact" className="h-screen flex items-center justify-center">
-          <h2 className="text-5xl font-bold">Contact</h2>
-        </section>
       </main>
     </div>
   );
